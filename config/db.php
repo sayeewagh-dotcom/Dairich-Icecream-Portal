25d09cc5656424889d49a5e74be1490661298659
Ojasvi1304 <ojasvijaiswal13@gmail.com>
2026-04-16 23:03:24 +0530

    your changes

--- config/db.php
+++ config/db.php
@@ -12,7 +12,7 @@ function db(): PDO {
         $port     = '5432';
         $dbname   = 'dairich';
         $user     = 'postgres';      // your pgAdmin username
-        $password = '#Sayee212';              // your pgAdmin password
+        $password = '';              // your pgAdmin password
 
         $pdo = new PDO(
             "pgsql:host=$host;port=$port;dbname=$dbname",
@@ -27,3 +27,4 @@ function db(): PDO {
 
     return $pdo;
 }
+?>
